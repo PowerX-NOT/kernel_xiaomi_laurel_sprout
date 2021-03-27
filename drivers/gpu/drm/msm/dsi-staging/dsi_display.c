15711e5121a5ad3eb8ed941659709b6692ca2e78
Albert I <kras@raphielgang.org>
2021-03-28 00:25:14 +0800

    drm/msm/dsi-staging: Create symlink of active panel as dsi-display-primary
    In order to support mia3fod implementation, current implementation of
    fingerprint under display HAL expects path to primary display as
    qcom,dsi-display-primary. However, on msm-4.9 dsi-staging normally creates
    sysfs under current active display during boot that'll never be same for
    all devices.
    
    Normally, commit 10e9b127e1a8 ("drm/msm/dsi-staging: optimize dsi probe")
    from msm-4.14 needs to be backported. However, due to potential headaches
    will be put when doing so and how it'll outperform the necessity of
    the backport, let's just create a kernfs symlink of current active display
    as qcom,dsi-display-primary and leave it alone.
    
    Test: Compiled, device boots, node symlink exists
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I4afa98ad95a65b270108692b162c76020f9f9695

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -19,6 +19,7 @@
 #include <linux/of_gpio.h>
 #include <linux/err.h>
 #include <linux/msm_drm_notify.h>
+#include <linux/kernfs.h>
 
 #include "msm_drv.h"
 #include "sde_connector.h"
@@ -59,6 +60,8 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{.compatible = "qcom,dsi-display"},
 	{}
 };
+
+static struct kernfs_node *primary_link;
 
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
@@ -5060,6 +5063,22 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 	int rc = 0;
 	struct device *dev = &display->pdev->dev;
 
+	if (dev->parent) {
+		struct kernfs_node *dsi_node = dev->kobj.sd;
+
+		kernfs_get(dsi_node);
+
+		primary_link =
+			kernfs_create_link(dev->parent->kobj.sd,
+					   "soc:qcom,dsi-display-primary",
+					   dsi_node);
+		if (IS_ERR(primary_link))
+			pr_err("[%s] unable to create dsi-display symlink\n",
+			       display->name);
+
+		kernfs_put(dsi_node);
+	}
+
 	rc = sysfs_create_group(&dev->kobj, &display_fs_attrs_group);
 	if (rc) {
 		pr_err("[%s] failed to create display device attributes\n",
@@ -5079,7 +5098,7 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 	}
 
 	pr_debug("[%s] dsi_display_sysfs_init:%d,panel mode:%d\n",
-		display->name, rc, display->panel->panel_mode);	
+		display->name, rc, display->panel->panel_mode);
 
 	return 0;
 
@@ -5098,6 +5117,10 @@ static int dsi_display_sysfs_deinit(struct dsi_display *display)
 			&dynamic_dsi_clock_fs_attrs_group);
 
 	sysfs_remove_group(&dev->kobj, &display_fs_attrs_group);
+
+	if (!IS_ERR_OR_NULL(primary_link))
+		kernfs_remove_by_name(primary_link->parent,
+				      primary_link->name);
 
 	return 0;
 
