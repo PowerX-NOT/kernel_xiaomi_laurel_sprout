5036eef680a369427bf9d4b8467c1fd2779aa993
Ivan Vecera <ivan@cera.cz>
2021-12-16 16:04:32 +0100

    drm/msm/dsi-staging: Add support for HBM
    Add support for HBM to implement Sunlight Enhancement LiveDisplay
    feature. The HBM knob also decide which variant of doze mode is used.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I684c04dfde26323f8bcd2cf8c2ddd0c501caa270

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -219,6 +219,7 @@ struct dsi_panel {
 
 	bool sync_broadcast_en;
 	bool doze_enabled;
+	bool hbm_enabled;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
 };
@@ -340,5 +341,18 @@ struct dsi_panel *dsi_panel_ext_bridge_get(struct device *parent,
 int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
+
+static inline bool dsi_panel_is_hbm_enabled(struct dsi_panel *panel)
+{
+	bool status;
+
+	dsi_panel_acquire_panel_lock(panel);
+	status = panel->hbm_enabled;
+	dsi_panel_release_panel_lock(panel);
+
+	return status;
+}
+
+int dsi_panel_set_hbm_enabled(struct dsi_panel *panel, bool status);
 
 #endif /* _DSI_PANEL_H_ */
