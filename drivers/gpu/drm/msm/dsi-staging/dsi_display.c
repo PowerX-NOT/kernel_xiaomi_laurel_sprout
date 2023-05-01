1996110e81828772fbe97b6ea10dfd9f4edd306d
Ivan Vecera <ivan@cera.cz>
2023-05-01 18:01:37 +0200

    drm/msm/dsi-staging: Do not expose unused fod_pressed attribute
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5092,45 +5092,6 @@ static ssize_t dc_dimming_store(struct device *dev,
 	return count;
 }
 
-static ssize_t fod_pressed_show(struct device *dev,
-				struct device_attribute *attr,
-				char *buf)
-{
-	struct dsi_display *display = dev_get_drvdata(dev);
-
-	if (!display->panel) {
-		pr_err("Invalid display\n");
-		return -EINVAL;
-	}
-
-	return snprintf(buf, PAGE_SIZE, "%d\n",
-			dsi_panel_is_fod_pressed(display->panel));
-}
-
-static ssize_t fod_pressed_store(struct device *dev,
-				 struct device_attribute *attr,
-				 const char *buf, size_t count)
-{
-	struct dsi_display *display = dev_get_drvdata(dev);
-	bool pressed;
-	int rc;
-
-	if (!display->panel) {
-		pr_err("Invalid display\n");
-		return -EINVAL;
-	}
-
-	rc = kstrtobool(buf, &pressed);
-	if (rc) {
-		pr_err("Failed to parse value, rc=%d\n", rc);
-		return rc;
-	}
-
-	dsi_panel_set_fod_pressed(display->panel, pressed);
-
-	return count;
-}
-
 static ssize_t fod_ui_show(struct device *dev, struct device_attribute *attr,
 			   char *buf)
 {
@@ -5186,13 +5147,11 @@ static ssize_t hbm_store(struct device *dev, struct device_attribute *attr,
 }
 
 static DEVICE_ATTR_RW(dc_dimming);
-static DEVICE_ATTR_RW(fod_pressed);
 static DEVICE_ATTR_RO(fod_ui);
 static DEVICE_ATTR_RW(hbm);
 
 static struct attribute *display_fs_attrs[] = {
 	&dev_attr_dc_dimming.attr,
-	&dev_attr_fod_pressed.attr,
 	&dev_attr_fod_ui.attr,
 	&dev_attr_hbm.attr,
 	NULL,
